f48a296572d25fc30e898b0095ab23fa06da3254
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-03-12 20:14:04 -0300

    Wallet/Header - Atualizaçao do numero de despesas totais

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,24 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  state = {
+    sumValue: () => {
+      const { expenses } = this.props;
+      if (expenses.length >= 1) {
+        const summedValue = expenses
+          .map((el) => {
+            const selectedCurrency = el.exchangeRates[el.currency].ask;
+            return parseFloat((el.value * selectedCurrency));
+          })
+          .reduce((partialSum, curNumber) => partialSum + curNumber);
+        return (summedValue);
+      }
+    },
+  };
+
   render() {
     const { email } = this.props;
+    const { sumValue } = this.state;
     return (
       <div>
         <Link to="/">Home</Link>
@@ -15,7 +31,7 @@ class Header extends Component {
         </p>
         <p>
           Despesas totais:
-          <span data-testid="total-field">{0}</span>
+          <span data-testid="total-field">{sumValue()}</span>
           {' '}
           <span data-testid="header-currency-field">BRL</span>
         </p>
@@ -26,10 +42,12 @@ class Header extends Component {
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  expenses: state.wallet.expenses,
 });
 
 Header.propTypes = {
   email: propTypes.string.isRequired,
+  expenses: propTypes.arrayOf(propTypes.shape).isRequired,
 };
 
 export default connect(mapStateToProps)(Header);
