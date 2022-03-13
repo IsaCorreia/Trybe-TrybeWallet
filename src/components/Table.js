74c61b26896f34bdd8fc83789c2abe106724bc30
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-03-13 17:21:56 -0300

    Wallet - Conteudo da tabela dinamica

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,37 @@
+import propTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Table extends Component {
+  genExpenseTableContent = () => {
+    const { expenses } = this.props;
+    return expenses.map((expense, index) => {
+      const { description, tag, method, value, currency, exchangeRates } = expense;
+      const currencies = exchangeRates[currency].name.split('/');
+      const roundedValue = (Math.round(value * 100) / 100).toFixed(2);
+      const roundedExchange = (
+        Math.round(exchangeRates[currency].ask * 100) / 100
+      ).toFixed(2);
+      const roundedExchangedValue = (
+        Math.round(value * exchangeRates[currency].ask * 100) / 100
+      ).toFixed(2);
+      return (
+        <tr key={ index }>
+          <td>{description}</td>
+          <td>{tag}</td>
+          <td>{method}</td>
+          <td>{roundedValue}</td>
+          <td>{currencies[0]}</td>
+          <td>{roundedExchange}</td>
+          <td>{roundedExchangedValue}</td>
+          <td>Real</td>
+        </tr>
+      );
+    });
+  };
+
   render() {
+    const { expenses } = this.props;
     return (
       <table>
         <thead>
@@ -17,9 +47,19 @@ class Table extends Component {
             <th>Editar/Excluir</th>
           </tr>
         </thead>
+
+        {expenses.length >= 1 && <tbody>{this.genExpenseTableContent()}</tbody>}
       </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: propTypes.arrayOf(propTypes.shape).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
